55dd9aeb70c8b57d14699a1086803251d73f1a1b
Jerry Meng <jmeng64@gmail.com>
2024-07-26 17:26:15 -0400

    7/26/24 created log file.

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -1,34 +1,32 @@
-import os
-import json 
-import traceback
-import pandas as pd 
-from dotenv import load_dotenv
+import os, json, pandas as pd
+
 from src.mcqgenerator.utils import read_file, get_table_data 
 from src.mcqgenerator.logger import logging 
 
-
 # import necessary packages from langchain 
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
+from langchain.llms import OpenAI 
+from langchain.prompts import PromptTemplate 
+from langchain.chains import LLMChain 
+from langchain.chains import SequentialChain 
 
+
+#from langchain.chat_models import ChatOpenAI
+# LangChainDeprecationWarning: The class `langchain_community.chat_models.openai.ChatOpenAI` was deprecated in langchain-community 0.0.10 and will be removed in 0.2.0. 
+# An updated version of the class exists in the langchain-openai package and should be used instead. To use it run `pip install -U langchain-openai` and import as `from langchain_openai import ChatOpenAI`.
 from langchain_openai import ChatOpenAI
-from langchain.callbacks import get_openai_callback
 
 
-# load env variables from the .env file. 
-load_dotenv() 
-
+from dotenv import load_dotenv
+load_dotenv() # taking env variables from .env file. 
 mykey=os.environ["OPENAI_API_KEY"]
-# temperature   0 - more determinisic.   2 - more randomness 
-llm=ChatOpenAI(openai_api_key=mykey, model_name="gpt-3.5-turbo", temperature=0.7)
+models=["gpt-3.5", "gpt-3.5-turbo", "gpt-4", "gpt-4-turbo"]  #  model choices 
 
+# temperature   0 - more determinisic.   2 - more randomness 
+llm=ChatOpenAI(openai_api_key=mykey, model_name="gpt-3.5-turbo", temperature=0.5)
 
 
 template="""
-Text:{text}
-You are an expert MCQ maker. Given the above text, it is your job to \
+You are an expert MCQ maker. It is your job to \
 create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
 Make sure the questions are not repeated and check all the questions to be conforming the text as well.
 Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
@@ -39,7 +37,7 @@ Ensure to make {number} MCQs
 """
 
 quiz_generation_prompt = PromptTemplate(
-    input_variables=["text", "number", "subject", "tone", "response_json"],
+    input_variables=["number", "subject", "tone", "response_json"],
     template=template 
     )
 
@@ -61,42 +59,7 @@ quiz_evaluation_prompt = PromptTemplate(input_varibles = ["subject", "quiz"], te
 review_chain = LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key = "review", verbose = True)
 
 # this is an overall chain where we run the two chains in sequence 
-generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["text", "number", "subject", "tone", "response_json"],
+generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["number", "subject", "tone", "response_json"],
                                         output_variables=["quiz", "review"], verbose=True)
-
-
-file_path=r"G:\My Drive\backup jerry-200k(master copy)\AI\project_MCQ\data_geometry.txt"
-
-with open(file_path, 'r') as file: 
-    TEXT=file.read() 
-    
-file_path=r"G:\My Drive\backup jerry-200k(master copy)\AI\project_MCQ\Response.json"
-
-with open(file_path, 'r') as file: 
-    RESPONSE_JSON=file.read() 
-
-
-NUMBER=5
-SUBJECT="Geometry" 
-TONE="Difficult"
-
-#https://python.langchain.com/docs/modules/model_io/llms/token_usage_tracking
-
-#How to setup Token Usage Tracking in LangChain
-with get_openai_callback() as cb:
-    response=generate_evaluate_chain(
-        {
-            "text": TEXT,
-            "number": NUMBER,
-            "subject":SUBJECT,
-            "tone": TONE,
-            "response_json": json.dumps(RESPONSE_JSON)
-        }
-    )
-    
-
-
-
-
 
 
